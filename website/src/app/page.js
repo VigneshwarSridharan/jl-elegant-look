bd663716a2da617dea5ea49e81ff98503acae3f3
viky.viky884 <viky.viky884@gmail.com>
2024-04-14 19:31:41 +0000

    changes

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -1,204 +1,68 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Slider from "./components/Slider";
 
 export default function Home() {
   return (
     <>
-      <div className="holder fullwidth full-nopad mt-0">
+      <Slider />
+
+      <div className="holder holder-mt-xsmall">
         <div className="container">
-          <div className="bnslider-wrapper">
-            <div
-              className="bnslider bnslider--lg keep-scale"
-              id="bnslider-001"
-              data-slick='{"arrows": true, "dots": true}'
-              data-autoplay="false"
-              data-speed="12000"
-              data-start-width="1917"
-              data-start-height="764"
-              data-start-mwidth="1550"
-              data-start-mheight="1000"
-            >
-              <div className="bnslider-slide">
-                <div
-                  className="bnslider-image-mobile lazyload"
-                  data-bgset="/images/slider/slide-fashion-02-m.png"
-                ></div>
-                <div
-                  className="bnslider-image lazyload"
-                  data-bgset="/images/slider/slide-fashion-02.png"
-                ></div>
-                <div className="bnslider-text-wrap bnslider-overlay">
-                  <div className="bnslider-text-content txt-middle txt-right txt-middle-m txt-center-m">
-                    <div className="bnslider-text-content-flex">
-                      <div
-                        className="bnslider-vert w-s-60 w-ms-100"
-                        style={{ padding: "0px" }}
-                      >
-                        <div
-                          className="bnslider-text order-1 mt-sm bnslider-text--md text-center data-ini"
-                          data-animation="fadeInUp"
-                          data-animation-delay="800"
-                          data-fontcolor="#282828"
-                          data-fontweight="700"
-                          data-fontline="1.5"
-                        >
-                          Best Price This Year
-                        </div>
-                        <div
-                          className="bnslider-text order-2 mt-sm bnslider-text--xs text-center data-ini"
-                          data-animation="fadeInUp"
-                          data-animation-delay="1000"
-                          data-fontcolor="#7c7c7c"
-                          data-fontweight="400"
-                          data-fontline="1.5"
-                        >
-                          eCommerce HTML Template
-                        </div>
-                        <div
-                          className="btn-wrap text-center order-4 mt-md"
-                          data-animation="fadeIn"
-                          data-animation-delay="2000"
-                          style={{ opacity: 1 }}
-                        >
-                          <a
-                            href="https://bit.ly/3eJX5XE"
-                            target="_blank"
-                            className="btn"
-                          >
-                            Shop now
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="bnslider-slide">
-                <div
-                  className="bnslider-image-mobile lazyload"
-                  data-bgset="/images/slider/slide-fashion-01-m.png"
-                ></div>
-                <div
-                  className="bnslider-image lazyload"
-                  data-bgset="/images/slider/slide-fashion-01.png"
-                ></div>
-                <div className="bnslider-text-wrap bnslider-overlay">
-                  <div className="bnslider-text-content txt-middle txt-left txt-middle-m txt-center-m">
-                    <div className="bnslider-text-content-flex">
-                      <div
-                        className="bnslider-vert w-s-60 w-ms-100"
-                        style={{ padding: "0px" }}
-                      >
-                        <div
-                          className="bnslider-text order-1 mt-0 bnslider-text--md text-center data-ini"
-                          data-animation="fadeInUp"
-                          data-animation-delay="800"
-                          data-fontcolor="#282828"
-                          data-fontweight="700"
-                          data-fontline="1.5"
-                        >
-                          Best Rated Theme 2020
-                        </div>
-                        <div
-                          className="bnslider-text order-2 mt-sm bnslider-text--xs text-center data-ini"
-                          data-animation="fadeInUp"
-                          data-animation-delay="1000"
-                          data-fontcolor="#7c7c7c"
-                          data-fontweight="400"
-                          data-fontline="1.5"
-                        >
-                          eCommerce HTML Template
-                        </div>
-                        <div
-                          className="btn-wrap text-center order-4 mt-md"
-                          data-animation="fadeIn"
-                          data-animation-delay="2000"
-                          style={{ opacity: 1 }}
-                        >
-                          <a
-                            href="https://bit.ly/3eJX5XE"
-                            target="_blank"
-                            className="btn"
-                          >
-                            Shop now
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="bnslider-slide slick-slide is-paused"
-                data-autoplay="true"
-                data-video-type="video"
-                data-slick-index="2"
-                aria-hidden="true"
-                style={{
-                  width: "1903px",
-                  position: "relative",
-                  left: "-3806px",
-                  top: "0px",
-                  zIndex: 998,
-                  opacity: 0,
-                  transition: "opacity 500ms ease 0s",
-                }}
-                id="slick-slide02"
+          <div className="row vert-margin-small">
+            <div className="col-sm">
+              <a
+                href="category.html"
+                className="collection-grid-3-item image-hover-scale"
               >
-                <div className="video-wrap">
-                  <video playsInline="" loop="" preload="auto">
-                    <source
-                      src="https://cdn.shopify.com/s/files/1/0201/0942/6742/files/video-girl.mp4?v=1594287483"
-                      type="video/mp4"
-                    />
-                  </video>
-                  <div className="video-control visible">
-                    <div className="video-play js-video-slider-play">
-                      <i className="icon-play"></i>
-                    </div>
-                    <div className="video-stop js-video-slider-stop">
-                      <i className="icon-pause"></i>
-                    </div>
-                  </div>
+                <div
+                  className="collection-grid-3-item-img image-container"
+                  style={{ paddingBottom: "93.68%" }}
+                >
+                  <img
+                    src="https://source.unsplash.com/random/720x675?girls-fashion$1"
+                    alt="Banner"
+                  />
+                  {/* <img
+                    src="https://source.unsplash.com/random/?Cryptocurrency&1"
+                    alt="Banner"
+                  /> */}
+                  {/* <div className="foxic-loader"></div> */}
                 </div>
-                <div className="bnslider-text-wrap bnslider-overlay">
-                  <div className="bnslider-text-content txt-middle txt-center txt-middle-m txt-center-m">
-                    <div className="bnslider-text-content-flex">
-                      <div className="bnslider-vert" style={{ padding: "0px" }}>
-                        <div
-                          className="bnslider-text order-1 mt-sm bnslider-text--md text-center data-ini"
-                          data-animation="fadeInUp"
-                          data-animation-delay="800"
-                          data-fontcolor="#282828"
-                          data-fontweight="700"
-                          data-fontline="1.5"
-                        >
-                          Live life in Comfort
-                        </div>
-                        <div
-                          className="btn-wrap text-center order-4 mt-md"
-                          data-animation="fadeIn"
-                          data-animation-delay="2000"
-                          style={{ opacity: 1 }}
-                        >
-                          <a href="#" target="_self" className="btn">
-                            {" "}
-                            Shop now{" "}
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                <div className="collection-grid-3-caption-bg">
+                  <h3 className="collection-grid-3-title">Accessories</h3>
+                  <h4 className="collection-grid-3-subtitle">
+                    The&nbsp;Best&nbsp;Look&nbsp;Anywhere
+                  </h4>
                 </div>
-              </div>
+              </a>
             </div>
-            <div className="bnslider-arrows container-fluid">
-              <div></div>
+            <div className="col-sm">
+              <a
+                href="category.html"
+                className="collection-grid-3-item image-hover-scale"
+              >
+                <div
+                  className="collection-grid-3-item-img image-container"
+                  style={{ paddingBottom: "93.68%" }}
+                >
+                  <img
+                    src="https://source.unsplash.com/random/720x675?fashion%20girl$2"
+                    alt="Banner"
+                  />
+                </div>
+                <div className="collection-grid-3-caption-bg">
+                  <h3 className="collection-grid-3-title">Fashion</h3>
+                  <h4 className="collection-grid-3-subtitle">
+                    Live&nbsp;According&nbsp;to&nbsp;Fashion
+                  </h4>
+                </div>
+              </a>
             </div>
-            <div className="bnslider-dots container-fluid"></div>
           </div>
         </div>
       </div>
+
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta cum
         consequatur provident earum aliquid quibusdam voluptas excepturi libero
