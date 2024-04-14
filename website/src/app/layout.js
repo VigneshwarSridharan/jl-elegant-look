d89c6f9e0ac17545bbb345d454ad676b3e318333
viky.viky884 <viky.viky884@gmail.com>
2024-04-14 16:08:52 +0000

    changes

--- website/src/app/layout.js
+++ website/src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import "./globals.css";
 
 export const metadata = {
@@ -22,15 +23,15 @@ export default function RootLayout({ children }) {
         <link
           rel="shortcut icon"
           type="image/x-icon"
-          href="images/favicon.ico"
+          href="/images/favicon.ico"
         />
         {/* <!-- Vendor CSS --> */}
-        <link href="css/vendor/bootstrap.min.css" rel="stylesheet" />
-        <link href="css/vendor/vendor.min.css" rel="stylesheet" />
+        <link href="/css/vendor/bootstrap.min.css" rel="stylesheet" />
+        <link href="/css/vendor/vendor.min.css" rel="stylesheet" />
         {/* <!-- Custom styles for this template --> */}
-        <link href="css/style.css" rel="stylesheet" />
+        <link href="/css/style.css" rel="stylesheet" />
         {/* <!-- Custom font --> */}
-        <link href="fonts/icomoon/icons.css" rel="stylesheet" />
+        <link href="/fonts/icomoon/icons.css" rel="stylesheet" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="has-smround-btns has-loader-bg equal-height">
+        <Header />
         {children}
       </body>
     </html>
--- website/src/components/Header.js
+++ website/src/components/Header.js
@@ -1,23 +1,79 @@
 const HeaderContent = ({ isSticky }) => {
   return (
-    <div class={`hdr-content ${isSticky ? "hdr-content-sticky" : ""}`}>
-      <div class="container">
-        <div class="row">
-          <div class="col-auto show-mobile">
+    <div className={`hdr-content ${isSticky ? "hdr-content-sticky" : ""}`}>
+      <div className="container">
+        <div className="row">
+          <div className="col-auto show-mobile">
             {/* <!-- Menu Toggle --> */}
-            <div class="menu-toggle">
-              <a href="#" class="mobilemenu-toggle">
-                <i class="icon-menu"></i>
+            <div className="menu-toggle">
+              <a href="#" className="mobilemenu-toggle">
+                <i className="icon-menu"></i>
               </a>
             </div>
-            {/* <!-- /Menu Toggle --> */}
-            <div class="col-auto hdr-logo">
-              <a href="index.html" class="logo">
-                <img
-                  srcSet="/images/logo.png 1x, /images/logo2x.png 2x"
-                  alt="Logo"
-                />
-              </a>
+          </div>
+          {/* <!-- /Menu Toggle --> */}
+          <div className="col-auto hdr-logo">
+            <a href="index.html" className="logo">
+              <img
+                srcSet="/images/logo.png 1x, /images/logo2x.png 2x"
+                alt="Logo"
+              />
+            </a>
+          </div>
+          <div
+            className={`hdr-nav hide-mobile ${
+              isSticky
+                ? "nav-holder-s"
+                : "nav-holder justify-content-center px-4"
+            }`}
+          >
+            <ul className="mmenu">
+              <li>
+                <a href="category.html">Home</a>
+              </li>
+              <li>
+                <a href="category.html">About Us</a>
+              </li>
+              <li>
+                <a href="category.html">Products</a>
+              </li>
+              <li>
+                <a href="category.html">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="hdr-links-wrap col-auto ml-auto">
+            <div className="hdr-inline-link">
+              <div className="search_container_desktop">
+                <div className="dropdn dropdn_search dropdn_fullwidth">
+                  <a href="#" className="dropdn-link js-dropdn-link only-icon">
+                    <i className="icon-search"></i>
+                    <span className="dropdn-link-txt">Search</span>
+                  </a>
+                  <div className="dropdn-content">
+                    <div className="container">
+                      <form
+                        method="get"
+                        action="#"
+                        className="search search-off-popular"
+                      >
+                        <input
+                          name="search"
+                          type="text"
+                          className="search-input input-empty"
+                          placeholder="What are you looking for?"
+                        />
+                        <button type="submit" className="search-button">
+                          <i className="icon-search"></i>
+                        </button>
+                        <a href="#" className="search-close js-dropdn-close">
+                          <i className="icon-close-thin"></i>
+                        </a>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -28,11 +84,13 @@ const HeaderContent = ({ isSticky }) => {
 
 const Header = () => {
   return (
-    <header class="hdr-wrap">
+    <header className="hdr-wrap">
       <HeaderContent isSticky />
-      <div class="hdr">
+      <div className="hdr">
         <HeaderContent />
       </div>
     </header>
   );
 };
+
+export default Header;
