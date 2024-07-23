5f7e190636002735902519e0975224c062da3bf8
viky.viky884 <viky.viky884@gmail.com>
2024-07-23 17:37:40 +0000

    changes

--- website/src/app/products/[category]/components/ListProducts.js
+++ website/src/app/products/[category]/components/ListProducts.js
@@ -6,7 +6,7 @@ const ListProducts = ({ products = [] }) => {
       {/* <!-- Products Grid --> */}
       <div className="product-listing data-to-show-3 data-to-show-md-3 data-to-show-sm-2 js-category-grid prd-grid">
         {products.map(({ id, attributes: product }) => {
-          return <Product key={id} {...product} />;
+          return <Product key={id} {...product} id={id} />;
         })}
       </div>
     </div>
